15f07bb76034a82087df02f4338043fad1db53dc
Mark Gass <mkgass@meredith.edu>
2023-02-18 19:50:05 -0500

    connect mid-tier to front-end in marker-id search

---
+++ routes/form.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+var express = require('express');
+const { request } = require('../app');
+
+var router = express.Router();
+
+/* Form to search for marker ID: just gather query parameter for use in redirect. */
+router.get('/searchById/submit', async function(req, res, next) {
+  var tgtURL = `/marker/${req.query.markerid}`;
+  res.redirect(307, tgtURL);
+});
+
+module.exports = router;
