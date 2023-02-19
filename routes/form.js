776dc68a9dc8cc99e5799a480b800581c2d69a28
Mark Gass <mkgass@meredith.edu>
2023-02-18 20:03:22 -0500

    allow lowercase; return to search page

--- routes/form.js
+++ routes/form.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /* Form to search for marker ID: just gather query parameter for use in redirect. */
 router.get('/searchById/submit', async function(req, res, next) {
-  var tgtURL = `/marker/${req.query.markerid}`;
+  var tgtURL = `/marker/${req.query.markerid.toUpperCase()}`;
   res.redirect(307, tgtURL);
 });
 
