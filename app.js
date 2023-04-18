886b94003212bb6ddf980c6377b4021270f82992
Mark Gass <mkgass@meredith.edu>
2023-04-18 12:27:07 -0400

    first mocha tests

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+//don't show the log during mocha tests
+if(process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
